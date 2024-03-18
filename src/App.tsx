cc2df53153c2037c4a81e3e16235fbfa51ad9d03
João Inácio Neto <birobirobiro.dev@gmail.com>
2024-03-18 20:41:53 +0000

    mvp

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,177 @@
-import CountBtn from "@/components/CountBtn";
-import ReactSVG from "@/assets/react.svg";
-import { Badge } from "@/components/ui/badge";
+import { useEffect, useState } from "react";
 import { ThemeProvider } from "@/components/theme-provider";
+import {
+  Card,
+  CardHeader,
+  CardTitle,
+  CardDescription,
+  CardContent,
+  CardFooter,
+} from "./components/ui/card";
+import { ScrollArea } from "./components/ui/scroll-area";
+import { websites } from "@/data/sites";
+import { Skeleton } from "./components/ui/skeleton";
+
+type Component = {
+  id: string;
+  name: string;
+  url: string;
+  status: string;
+}
+
+type Status = {
+  indicator: string;
+  description: string;
+}
+
+type Page = {
+  id: string;
+  name: string;
+  url: string;
+  status: string;
+  updated_at: string;
+}
+
+type IncidentsProps = {
+  components: Component[];
+  status: Status;
+  page: Page;
+}
 
 function App() {
+  const [websiteData, setWebsiteData] = useState<IncidentsProps[]>([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        setLoading(true);
+        const data = await Promise.all(
+          websites.map((website) => getStatus(website.url))
+        );
+        console.log(data);
+        setWebsiteData(data);
+        setLoading(false);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+        setLoading(false);
+      }
+    }
+    fetchData();
+  }, []);
+
+  const getStatus = async (url: string) => {
+    try {
+      const response = await fetch(url);
+      if (!response.ok) {
+        throw new Error("Network response was not ok.");
+      }
+      const data = await response.json();
+      return data;
+    } catch (error) {
+      console.error("Error getting status:", error);
+      return { status: { description: "Error fetching status" } };
+    }
+  };
+
+  const getStatusColor = (status: string) => {
+    switch (status) {
+      case "operational":
+        return "text-green-500";
+      case "unstable":
+        return "text-yellow-500";
+      case "down":
+        return "text-red-500";
+      default:
+        return "text-black";
+    }
+  };
+
   return (
     <ThemeProvider defaultTheme="system" storageKey="vite-ui-theme">
-      <main className="flex flex-col items-center justify-center h-screen">
-        <div className="flex flex-col items-center gap-y-4">
-          <div className="inline-flex items-center gap-x-4">
-            <img src={ReactSVG} alt="React Logo" className="w-32" />
-            <span className="text-6xl">+</span>
-            <img src={"/vite.svg"} alt="Vite Logo" className="w-32" />
+      <main className="flex flex-col items-center justify-center h-full py-20 px-10">
+        <div className="flex flex-col text-center mb-10 gap-2">
+          <h1 className="text-3xl font-bold">DevDownDetector</h1>
+
+          <span>
+            This app list all the services that are currently down and use
+            service{"  "}
+            <a
+              className="underline text-muted-foreground"
+              href="https://status.atlassian.com/"
+              target="_blank"
+            >
+              Atlassian Status Page
+            </a>
+          </span>
+
+          <div className="flex gap-2 items-center justify-center">
+            <span className="text-green-500">operational</span>
+            <span className="text-yellow-500">unstable</span>
+            <span className="text-red-500">down</span>
           </div>
-          <a
-            href="https://ui.shadcn.com"
-            rel="noopener noreferrer nofollow"
-            target="_blank"
-          >
-            <Badge variant="outline">shadcn/ui</Badge>
-          </a>
-          <CountBtn />
         </div>
+        <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3">
+          {websites.map((website, index) => (
+            <Card key={index}>
+              <CardHeader>
+                <CardTitle className="text-lg font-bold">
+                  {website.name}
+                </CardTitle>
+                <CardDescription
+                  className={`${websiteData[index]?.status.description !==
+                    "All Systems Operational"
+                    ? "text-yellow-500"
+                    : ""
+                    }`}
+                >
+                  {websiteData[index]?.status.description}
+                </CardDescription>
+              </CardHeader>
+              <CardContent>
+                <ScrollArea className="h-80">
+                  <ul>
+                    {websiteData[index]?.components.map((component) => (
+                      <li key={component.id}>
+                        {loading ? (
+                          <Skeleton className="h-6 w-full flex gap-4 flex-col my-2" />
+                        ) : (
+                          <div
+                            className={`
+            ${getStatusColor(component.status)}
+            flex gap-2 items-center
+            `}
+                          >
+                            <p>{component.name}</p>
+                            -
+                            <p>{component.status}</p>
+                          </div>
+                        )}
+                      </li>
+                    ))}
+                  </ul>
+                </ScrollArea>
+
+                <CardFooter className="justify-center mt-2">
+                  <p className="text-muted-foreground text-center">
+                    Last updated:{" "}
+                    {new Date(
+                      websiteData[index]?.page.updated_at
+                    ).toLocaleString("en-US")}
+                  </p>
+                </CardFooter>
+              </CardContent>
+            </Card>
+          ))}
+        </div>
+
+        <footer className="mt-10">
+          <p className="text-center text-muted-foreground">
+            <a href="https://birobirobiro.dev" target="_blank">
+              birobirobiro.dev
+            </a>
+          </p>
+        </footer>
       </main>
     </ThemeProvider>
   );
---
+++ src/data/sites.ts
@@ -0,0 +1,74 @@
+export const base_url_atlassian = "api/v2/summary.json";
+
+export const websites = [
+  {
+    name: "Notion",
+    url: `https://status.notion.so/${base_url_atlassian}`,
+  },
+  {
+    name: "Figma",
+    url: `https://status.figma.com/${base_url_atlassian}`,
+  },
+  {
+    name: "Discord",
+    url: `https://discordstatus.com/${base_url_atlassian}`,
+  },
+  {
+    name: "Vercel",
+    url: `https://www.vercel-status.com/${base_url_atlassian}`,
+  },
+  {
+    name: "GitHub",
+    url: `https://www.githubstatus.com/${base_url_atlassian}`,
+  },
+  {
+    name: "Render",
+    url: `https://status.render.com/${base_url_atlassian}`,
+  },
+  {
+    name: "Cloudinary",
+    url: `https://status.cloudinary.com/${base_url_atlassian}`,
+  },
+  {
+    name: "Jira",
+    url: `https://jira-software.status.atlassian.com/${base_url_atlassian}`,
+  },
+  {
+    name: "Open IA",
+    url: `https://status.openai.com/${base_url_atlassian}`,
+  },
+  {
+    name: "npm",
+    url: `https://status.npmjs.org/${base_url_atlassian}`,
+  },
+  {
+    name: "DataDog",
+    url: `https://status.datadoghq.com/${base_url_atlassian}`,
+  },
+  {
+    name: "LinkedIn",
+    url: `https://www.linkedin-status.com/${base_url_atlassian}`,
+  },
+  {
+    name: "Linode",
+    url: `https://status.linode.com/${base_url_atlassian}`,
+  },
+  {
+    name: "Digital Ocean",
+    url: `https://status.digitalocean.com/${base_url_atlassian}`,
+  },
+  {
+    name: "Miro",
+    url: `https://status.miro.com/${base_url_atlassian}`,
+  },
+
+  // Websites by Incident.io
+  // {
+  //   name: "Dub.co",
+  //   url: `https://status.dub.co/proxy/status.dub.co`,
+  // },
+  // {
+  //   name: "Linear",
+  //   url: `https://linearstatus.com/proxy/linearstatus.com`,
+  // }
+];
